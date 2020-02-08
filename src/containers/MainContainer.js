e2026e9bf9fb879a4326fab8374e6aaa355fa4fd
Steve Alex <steve.alex@hotmail.co.uk>
2020-02-08 20:49:50 +0000

    Created issue page + button to navigate there

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { HomePage } from '../pages/HomePage'
-import { BookingsPage } from '../pages/BookingsPage'
-import { ProfilePage } from '../pages/ProfilePage'
-import { InformationPage } from '../pages/InformationPage'
+import { HomePage } from "../pages/HomePage";
+import { BookingsPage } from "../pages/BookingsPage";
+import { ProfilePage } from "../pages/ProfilePage";
+import { InformationPage } from "../pages/InformationPage";
+import { IssuePage } from "../pages/IssuePage";
 
-export const MainContainer = () => {
+export const MainContainer = ({ history }) => {
   return (
     <div>
-      <Route path='/home'>
-        <HomePage/>
+      <Route path="/home">
+        <HomePage />
       </Route>
-      <Route path='/bookings'>
+      <Route path="/bookings">
         <BookingsPage />
       </Route>
-      <Route path='/profile'>
+      <Route path="/profile">
         <ProfilePage />
       </Route>
-      <Route path='/information'>
+      <Route path="/information">
         <InformationPage />
       </Route>
+      <Route path="/create-issue">
+        <IssuePage />
+      </Route>
+
     </div>
-  )
-}
+  );
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { ReportIssuePage } from './ReportIssuePage'
+import { Button } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+import "./pages.scss";
 
 export const HomePage = () => {
   const [selected, setSelected] = useState(undefined);
+
   return (
-    <div>
-      <button onClick={() => setSelected(true)}>Report an issue</button>
-      {selected &&
-        <ReportIssuePage />
-      }
+    <div className="homePageContainer">
+      {!selected && (
+        <div className="homePage">
+          <p>Hello User, welcome to West housing association</p>
+          <p>How can we help today?</p>
+          <Button
+            color="orange"
+            inverted
+            className="homePageButton"
+            onClick={() => setSelected(true)}
+          >
+            Report an issue
+          </Button>
+        </div>
+      )}
+      {selected && <Redirect to="/create-issue" />}
     </div>
   );
 };
