a028c3dadd80f926b64182dfc1e944fa191f1e81
Steve Alex <steve.alex@hotmail.co.uk>
2020-02-08 20:50:32 +0000

    Created navigation functionality

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Menu, Icon } from 'semantic-ui-react';
+import './components.scss'
 
-export const NavigationMenu = () => {
+export const NavigationMenu = ({ history }) => {
+  const [activeItem, setActiveItem] = useState("/home");
+
+  const handleClick = route => {
+    setActiveItem(route);
+    history.push(route);
+  };
+
   return (
-    <div>
-      Navigation
+    <div className="menuContainer">
+      <Menu icon="labeled" className="menu">
+        <Menu.Item
+          name="Home"
+          active={activeItem === "/home"}
+          onClick={() => handleClick("/home")}
+        >
+          <Icon name="home" />
+        </Menu.Item>
+        <Menu.Item
+          name="Bookings"
+          active={activeItem === "/booking"}
+          onClick={() => handleClick("/bookings")}
+        >
+          <Icon name="calendar" />
+        </Menu.Item>
+        <Menu.Item
+          name="Home"
+          active={activeItem === "/profile"}
+          onClick={() => handleClick("/profile")}
+        >
+          <Icon name="user" />
+        </Menu.Item>
+        <Menu.Item
+          name="Home"
+          active={activeItem === "/information"}
+          onClick={() => handleClick("/information")}
+        >
+          <Icon name="info" />
+        </Menu.Item>
+      </Menu>
     </div>
-  )
-}
+  );
+};
